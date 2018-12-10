4036985868e699a7014778e323e03430bb36f015
SteeringBIG <arway@inbox.ru>
2018-12-10 22:44:39 +0700

    update config

--- src/config.php
+++ src/config.php
@@ -1,24 +1,7 @@
-<?php
-return [ //Тестовая база ЦТО
-    'host' => 'mysql99.1gb.ru',
-    'db_name' => 'gb_cto_new',
-    'username' => 'gb_cto_new',
-    'password' => 'abez6a3eemz2',
-    'charset' => 'utf8'
+return [
+	'host' => 'localhost',
+	'db_name' => 'gb_cto_ok',
+	'username' => 'root',
+	'password' => '',
+	'charset' => 'utf8'
 ];
-
-//return [ //Рабочая база ЦТО
-//	'host' => 'mysql89.1gb.ru',
-//	'db_name' => 'gb_cto_ok',
-//	'username' => 'gb_cto_ok',
-//	'password' => 'a5ba8badahjk',
-//	'charset' => 'utf8'
-//];
-
-//return [
-//	'host' => 'localhost',
-//	'db_name' => 'gb_cto_ok',
-//	'username' => 'root',
-//	'password' => '',
-//	'charset' => 'utf8'
-//];
