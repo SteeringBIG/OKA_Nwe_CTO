b722123c42272b7057f952debc9e17ac8a02fff7
SteeringBIG <arway@inbox.ru>
2018-12-11 09:46:38 +0700

    Исключение файла config из индекса. Попытка: 2

---
+++ src/config.php
@@ -0,0 +1,16 @@
+<!--
+return [
+	'host' => 'localhost',
+	'db_name' => 'gb_cto_ok',
+	'username' => 'root',
+	'password' => '',
+	'charset' => 'utf8'
+];
+-->
+return [
+'host' => 'localhost',
+'db_name' => 'gb_cto_ok',
+'username' => 'root',
+'password' => '',
+'charset' => 'utf8'
+];
