db71965c87331e85635a7df78885a8e8c5108c70
SteeringBIG <arway@inbox.ru>
2019-01-14 00:19:42 +0700

    Страница 404

--- views/404.php
+++ views/404.php
@@ -1,9 +1,29 @@
-<div class="col">
-	<br>
+<div >
+    <div class="col-12">
+        <br>
+        <div class="alert alert-danger btn-block" role="alert">
+            <h1>404. Страница не найдена</h1>
+            <p class="desc">
+                Возможно, она была перемещена, или вы просто неверно указали адрес страницы.
+            </p>
+            <div class="links">
+                <a href="/app?mexcod=" <?= $_GET['comment'] ?> >
+                    Перейти на&nbsp;главную
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="col-12">
+        <div class="btn-block text-center">
+            
+            
+            <div class="">
+                <img src="/views/img/404.gif" width="300px">
+            </div>
+    
+        </div>
+    </div>
+    <div class="btn-block">
+    
+    </div>
 </div>
-<div class="col">
-	<div class="alert alert-danger btn-block" role="alert">
-		<h4 class="text-center">404 Page Not Found</h4>
-	</div>
-</div>
-
